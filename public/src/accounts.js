46662e4cea2f46bdf29a2b941d9fb3dfe8ba520c
Alex Whitmore <heyimalexw@gmail.com>
2020-12-15 12:27:14 -0500

    Save changes

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,8 +1,24 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+  for (let i = 0; i < accounts.length; i++) {
+    if (accounts[i].id === id) {
+      return accounts[i];
+    }
+  }
+}
 
-function sortAccountsByLastName(accounts) {}
+const sortAccountsByLastName = (accounts) =>
+  accounts.sort((a, b) => (a.name.last > b.name.last ? 1 : -1));
 
-function numberOfBorrows(account, books) {}
+function numberOfBorrows(account, books) {
+  let borrowCounter = 0;
+  const account = accounts;
+  for (let i = 0; i < books.length; i++) {
+    if (books[i].id === account.id) {
+      borrowCounter++;
+    }
+  }
+  return borrowCounter;
+}
 
 function booksInPossession(account, books, authors) {}
 
